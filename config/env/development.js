6f2ba6cc6e72ebb90c2a703b4eb8d9c5bf4a633a
sherifwahballa1 <sherifwahballa0@gmail.com>
2021-08-06 23:27:57 +0200

    add configurations

--- config/env/development.js
+++ config/env/development.js
@@ -1,13 +1,14 @@
 module.exports = {
-  dbURI: "mongodb://127.0.0.1:27017/uptime-down-monitoring",
-  SESSION_SECRET: "7fa6f51e-92ae-4f9c-aaa5-9080a003f254",
-  ORIGIN_CORS: ["http://localhost:4200"],
-  tokenValidationInDays: 365,
-  tempTokenDurationInHours: 1,
-  timeZone: "Africa/Cairo",
-  cronTime: 10,
-  email: {
-    user: process.env.User_Email,
-    pass: process.env.User_Password,
-  },
-};
+	dbURI: "mongodb://127.0.0.1:27017/uptime-down-monitoring",
+	SESSION_SECRET: "7fa6f51e-92ae-4f9c-aaa5-9080a003f254",
+	ORIGIN_CORS: ["http://localhost:4200", "http://192.168.1.55:4200"],
+	tokenValidationInDays: 365,
+	tempTokenDurationInHours: 1,
+	timeZone: "Africa/Cairo",
+	cronTime: 1,
+	appPushOverToken: "aowxetmn69qp7umvdxe19gs9vy7x75",
+	email: {
+		user: "asc.wargames@gmail.com",
+		pass: "@asc2020"
+	},
+}
