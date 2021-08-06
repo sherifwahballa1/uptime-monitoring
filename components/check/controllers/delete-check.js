9dfe8426d670c37dbd5716d8508f53e63b2a2c46
sherifwahballa1 <sherifwahballa0@gmail.com>
2021-08-07 01:33:00 +0200

    modify cron job

--- components/check/controllers/delete-check.js
+++ components/check/controllers/delete-check.js
@@ -1,32 +1,30 @@
-const mongoose = require('mongoose');
-const createError = require("http-errors")
+const mongoose = require("mongoose");
+const createError = require("http-errors");
 const Check = require("../check.model");
 const Report = require("./../../report/report.model");
 
 deleteCheck = async (req, res, next) => {
-    try {
-        let checkID = req.params.id;
-        if (!mongoose.Types.ObjectId.isValid(checkID))
-            return res.status(404).json({ message: 'Invalid check' });
+  try {
+    let checkID = req.params.id;
+    if (!mongoose.Types.ObjectId.isValid(checkID))
+      return res.status(404).json({ message: "Invalid check" });
 
-        let userId = mongoose.Types.ObjectId(req.userData._id);
-        
-        await Check.findOne({ _id: checkID, userId })
-            .select("_id name url isActive")
-            .orFail((err) => {
-                return next(createError(404, "Check not found"));
-            });
+    let userId = mongoose.Types.ObjectId(req.userData._id);
 
-        await Check.findByIdAndRemove(checkID);
-        await Report.deleteMany({ checkId: checkID });
-
-        return res
-            .status(200)
-            .json({ message: "Check deleted successfully" });
-
-    } catch (e) {
-        return res.status(500).json({ message: "Internal server error" });
+    let check = await Check.findOne({ _id: checkID, userId }).select(
+      "_id name url isActive"
+    );
+    if (!check) {
+      return next(createError(404, "Check not found"));
     }
-}
+
+    await Check.findByIdAndRemove(checkID);
+    await Report.deleteMany({ checkId: checkID });
+
+    return res.status(200).json({ message: "Check deleted successfully" });
+  } catch (e) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 module.exports = deleteCheck;
--- crons/jobs/checks-monitoring.js
+++ crons/jobs/checks-monitoring.js
@@ -5,9 +5,25 @@ const Report = require("../../components/report/report.model");
 const Email = require("../../modules/email");
 const axios = require("axios");
 const { pushNotificationService } = require("./../../notifications system");
+const e = require("express");
 let monitors = [];
 let lastChecks = [];
-let new_checks = [];
+// let new_checks = [];
+
+function initNewCheck(check) {
+  if (check.isActive === true) {
+    let monitor = new Monitor(
+      check,
+      axios,
+      Email,
+      Check,
+      Report,
+      pushNotificationService
+    );
+    monitors.push(monitor);
+    lastChecks.push(check);
+  }
+}
 
 module.exports = function (CronJob) {
   let job = new CronJob({
@@ -19,24 +35,39 @@ module.exports = function (CronJob) {
         select: "name email notifications",
       });
 
-      // TODO: Fix error
-      // if monitors > checks || monitors < checks
-      if (monitors.length != checks.length) {
-        if (checks.length !== lastChecks.length) {
-          new_checks = checks.slice(lastChecks.length, checks.length);
+      if (checks.length > 0 && monitors.length != checks.length) {
+        // can use redis here to cache current checks(monitors)
+        // first time
+        if (checks.length > monitors.length && monitors.length === 0) {
+          console.log("First");
+          // new_checks = checks.slice(lastChecks.length, checks.length);
+          checks.forEach((check) => {
+            initNewCheck(check);
+          });
+
+          // if new checks added
+        } else if (checks.length > monitors.length && monitors.length !== 0) {
+          let new_checks = checks.slice(monitors.length, checks.length);
           new_checks.forEach((check) => {
-            if (check.isActive === true) {
-              let monitor = new Monitor(
-                check,
-                axios,
-                Email,
-                Check,
-                Report,
-                pushNotificationService
-              );
-              monitors.push(monitor);
-              lastChecks.push(check);
-            }
+            initNewCheck(check);
+          });
+          // return the new checks after remove one or more
+        } else if (checks.length < monitors.length) {
+          let expiredChecks = lastChecks.filter((el) =>
+            checks.every((check2) => check2._id.toString() != el._id.toString())
+          );
+
+          expiredChecks.forEach((check) => {
+            let findMonitor = monitors.find(
+              (el) => el.check._id.toString() === check._id.toString()
+            );
+            findMonitor.stop();
+            monitors = monitors.filter((monitor) => {
+              return monitor.check._id.toString() !== check._id.toString();
+            });
+            lastChecks = lastChecks.filter((ex_check) => {
+              return check._id.toString() !== ex_check._id.toString();
+            });
           });
         }
       }
